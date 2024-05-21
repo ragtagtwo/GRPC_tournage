620121bf8fea4e4d728ce7a953b34f2ad5c2520d
ragtagtwo <rayenidoudi5@gmail.com>
2024-05-21 18:08:46 +0100

    grpc tournage

---
+++ src/main/java/com/yrrhelp/App.java
@@ -0,0 +1,13 @@
+package com.yrrhelp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/yrrhelp/AppTest.java
@@ -0,0 +1,10 @@
+package com.yrrhelp;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+}
